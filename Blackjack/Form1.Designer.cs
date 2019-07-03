0bddde987599e64ecd5aba023b693dd7345ebf96
Teemo <eru7921129@gmail.com>
2019-07-04 01:08:26 +1200

    Make the game meet rules of real-life Blackjack

--- Blackjack/Form1.Designer.cs
+++ Blackjack/Form1.Designer.cs
@@ -32,27 +32,30 @@
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.resultLabel = new System.Windows.Forms.Label();
-            this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.hitMeButton = new System.Windows.Forms.Button();
             this.resetButton = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
             this.SuspendLayout();
             // 
             // dealButton
             // 
-            this.dealButton.Location = new System.Drawing.Point(191, 12);
+            this.dealButton.Location = new System.Drawing.Point(12, 281);
             this.dealButton.Name = "dealButton";
             this.dealButton.Size = new System.Drawing.Size(75, 23);
             this.dealButton.TabIndex = 3;
-            this.dealButton.Text = "Deal";
+            this.dealButton.Text = "Start";
             this.dealButton.UseVisualStyleBackColor = true;
-            this.dealButton.Click += new System.EventHandler(this.dealButton_Click);
+            this.dealButton.Click += new System.EventHandler(this.startButton_Click);
             // 
             // pictureBox2
             // 
-            this.pictureBox2.Location = new System.Drawing.Point(95, 12);
+            this.pictureBox2.Location = new System.Drawing.Point(154, 137);
             this.pictureBox2.Name = "pictureBox2";
             this.pictureBox2.Size = new System.Drawing.Size(71, 96);
             this.pictureBox2.TabIndex = 2;
@@ -60,7 +63,7 @@
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(77, 137);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(71, 96);
             this.pictureBox1.TabIndex = 1;
@@ -69,32 +72,24 @@
             // resultLabel
             // 
             this.resultLabel.AutoSize = true;
-            this.resultLabel.Location = new System.Drawing.Point(188, 51);
+            this.resultLabel.Location = new System.Drawing.Point(149, 277);
             this.resultLabel.Name = "resultLabel";
             this.resultLabel.Size = new System.Drawing.Size(0, 13);
             this.resultLabel.TabIndex = 4;
             // 
-            // pictureBox3
-            // 
-            this.pictureBox3.Location = new System.Drawing.Point(12, 130);
-            this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(71, 96);
-            this.pictureBox3.TabIndex = 5;
-            this.pictureBox3.TabStop = false;
-            // 
             // hitMeButton
             // 
-            this.hitMeButton.Location = new System.Drawing.Point(285, 12);
+            this.hitMeButton.Location = new System.Drawing.Point(12, 315);
             this.hitMeButton.Name = "hitMeButton";
             this.hitMeButton.Size = new System.Drawing.Size(75, 23);
             this.hitMeButton.TabIndex = 6;
-            this.hitMeButton.Text = "Hit Me";
+            this.hitMeButton.Text = "Deal";
             this.hitMeButton.UseVisualStyleBackColor = true;
-            this.hitMeButton.Click += new System.EventHandler(this.hitMeButton_Click);
+            this.hitMeButton.Click += new System.EventHandler(this.DealButton_Click);
             // 
             // resetButton
             // 
-            this.resetButton.Location = new System.Drawing.Point(379, 12);
+            this.resetButton.Location = new System.Drawing.Point(12, 389);
             this.resetButton.Name = "resetButton";
             this.resetButton.Size = new System.Drawing.Size(75, 23);
             this.resetButton.TabIndex = 7;
@@ -102,15 +97,54 @@
             this.resetButton.UseVisualStyleBackColor = true;
             this.resetButton.Click += new System.EventHandler(this.resetButton_Click);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 354);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Player Stop";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.PlayerStopButton_Click);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(77, 12);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(71, 96);
+            this.pictureBox4.TabIndex = 10;
+            this.pictureBox4.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 183);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Player";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Banker";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::Blackjack.Properties.Resources.greenfelt;
-            this.ClientSize = new System.Drawing.Size(484, 281);
+            this.ClientSize = new System.Drawing.Size(936, 513);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.resetButton);
             this.Controls.Add(this.hitMeButton);
-            this.Controls.Add(this.pictureBox3);
             this.Controls.Add(this.resultLabel);
             this.Controls.Add(this.dealButton);
             this.Controls.Add(this.pictureBox2);
@@ -120,7 +154,7 @@
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -132,9 +166,12 @@
         private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.Button dealButton;
         private System.Windows.Forms.Label resultLabel;
-        private System.Windows.Forms.PictureBox pictureBox3;
         private System.Windows.Forms.Button hitMeButton;
         private System.Windows.Forms.Button resetButton;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- Blackjack/Form1.cs
+++ Blackjack/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -13,6 +14,7 @@ namespace Blackjack
     public partial class Form1 : Form
     {
         /* http://patorjk.com/software/taag/
+        //https://github.com/pda87/Blackjack
          
  ________  ___       ________  ________  ___  __          ___  ________  ________  ___  __       
 |\   __  \|\  \     |\   __  \|\   ____\|\  \|\  \       |\  \|\   __  \|\   ____\|\  \|\  \     
@@ -31,15 +33,24 @@ namespace Blackjack
 
         //CHANGE THE VALUE OF ALL FACE CARDS TO 10, AND THE ACE TO 11
 
-        List<Card> cardList = new List<Card>()
+        List<Card> playercardList = new List<Card>()
         {
             new Card() { Value  = 0, Name = "null", Image = "null" }
         };
-        
-        int cardSum = 0;
+
+        List<Card> bankercardList = new List<Card>()
+        {
+            new Card() { Value  = 0, Name = "null", Image = "null" }
+        };
+
+        int playercardSum = 0;
+        int bankercardSum = 0;
         Random random = new Random();
         List<int> usedCards = new List<int>();
-        
+        List<PictureBox> bankerbox = new List<PictureBox>();
+        List<PictureBox> playerbox = new List<PictureBox>();
+
+
         #region creationof52carddeck
 
         List<Card> deck = new List<Card>()
@@ -129,54 +140,75 @@ namespace Blackjack
             resetGame();
         }
 
-         private void dealButton_Click(object sender, EventArgs e)
+        //start the game 
+         private void startButton_Click(object sender, EventArgs e)
         {
-            if (cardSum > 0)
+            if (playercardSum > 0)
             {
                     resultLabel.Text = String.Format
-                    ("Click Reset");
+                    ("Already started.");
             }
 
             else
             {
-                displayCardBack(pictureBox3);
-                
-                cardSum = 0;
+                playercardSum = 0;
+                bankercardSum = 0;
+
+                #region init player
                 int randomCard1 = selectRandomCard();
                 Card card1 = deck[randomCard1];
                 usedCards.Add(randomCard1);
                 int randomCard2 = selectRandomCard();
 
-                if (randomCard1 == randomCard2) selectRandomCard();
-                else randomCard2 = 1 * randomCard2;
+
+                while (usedCards.Contains(randomCard2))
+                {
+                    randomCard2 = selectRandomCard();
+                }
+                randomCard2 = 1 * randomCard2;
                 
                 Card card2 = deck[randomCard2];
                 usedCards.Add(randomCard2);
 
-                cardList.Add(card1);
-                cardList.Add(card2);
+                playercardList.Add(card1);
+                playercardList.Add(card2);
                 
                 pictureBox1.ImageLocation = card1.Image;
                 pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
                 
                 pictureBox2.ImageLocation = card2.Image;
                 pictureBox2.SizeMode = PictureBoxSizeMode.AutoSize;
+                #endregion
+                #region init banker
+                int randomCard3 = selectRandomCard();
+                while (usedCards.Contains(randomCard3))
+                {
+                    randomCard3 = selectRandomCard();
+                }
+                randomCard3 = 1 * randomCard3;
+                Card card3 = deck[randomCard3];
+                usedCards.Add(randomCard3);
 
-                sumCards();
-                if (cardSum > 21)
+                bankercardList.Add(card3);
+
+                pictureBox4.ImageLocation = card3.Image;
+                pictureBox4.SizeMode = PictureBoxSizeMode.AutoSize;
+
+                #endregion
+
+                sumPlayerCards();
+
+                //sumCards();
+
+                if (playercardSum == 21)
                 {
                     resultLabel.Text = String.Format
-                        ("The sum of the cards is: {0}, you lose! Click Reset", cardSum);
+                        ("The sum of your cards is: {0}, you win!", playercardSum);
+                    MessageBox.Show("You win!", "", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    resetGame();
                 }
 
-                else if (cardSum == 21)
-                {
-                    resultLabel.Text = String.Format("The sum of the cards is: {0}, you win!",
-                   cardSum);
-                }
-                else
-                    resultLabel.Text = String.Format("The sum of the cards is: {0}",
-                       cardSum);
+
             }
         }
 
@@ -187,35 +219,71 @@ namespace Blackjack
             return randomCard;
         }
 
-        private int sumCards()
+        private void sumPlayerCards()
         {
-
-            for (int i = 0; i < cardList.Count; i++)
+            playercardSum = 0;
+            for (int i = 0; i < playercardList.Count; i++)
             {
-                cardSum += cardList[i].Value;
+                playercardSum += playercardList[i].Value;
             }
-            return cardSum;
-        }
 
-        private void hitMeButton_Click(object sender, EventArgs e)
-        {
-            if (cardSum == 0)
+            if(playercardSum > 21)
             {
-                resultLabel.Text = "Click the Deal button...";
-                displayCardBack(pictureBox3);
+                foreach(Card c in playercardList)
+                {
+                    if(c.Value == 11)
+                    {
+                        playercardSum -= 10;
+                        if(playercardSum <= 21)
+                        {
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        private void sumBankerCards()
+        {
+            bankercardSum = 0;
+            for (int i = 0; i < bankercardList.Count; i++)
+            {
+                bankercardSum += bankercardList[i].Value;
+            }
+            if (bankercardSum > 21)
+            {
+                foreach (Card c in bankercardList)
+                {
+                    if (c.Value == 11)
+                    {
+                        bankercardSum -= 10;
+                        if (bankercardSum <= 21)
+                        {
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        // give one card to player
+        private void DealButton_Click(object sender, EventArgs e)
+        {
+            if (playercardSum == 0)
+            {
+                resultLabel.Text = "Click the Start button...";
+                //displayCardBack(pictureBox3);
             }
 
             else
             {
-                if (cardSum > 100)
+                if (playercardSum > 100) //to be changed
                 {
                     resetGame();
-                    resultLabel.Text = "Resetting game... Click Deal when ready";
+                    resultLabel.Text = "Resetting game...";
                 }
 
                 else
                 {
-                    cardSum = 0;
+                    playercardSum = 0;
                     int randomCard = selectRandomCard();
                     Card card = deck[randomCard];
                     usedCards.Add(randomCard);
@@ -223,24 +291,34 @@ namespace Blackjack
                     if (usedCards.Contains(randomCard)) randomCard = selectRandomCard();
                     else randomCard = 1 * randomCard;
 
-                    pictureBox3.ImageLocation = card.Image;
-                    pictureBox3.SizeMode = PictureBoxSizeMode.AutoSize;
-                    cardList.Add(card);
-                    cardSum = sumCards();
-                    if (cardSum > 21)
-                    {
-                        resultLabel.Text = String.Format
-                            ("The sum of the cards is: {0}, you lose! Click Reset", cardSum);
-                    }
 
-                    else if (cardSum == 21)
+                    //player new card
+                    PictureBox p3 = new PictureBox();
+                    p3.Width = 71;
+                    p3.Height = 96;
+                    p3.Location = new Point(154 + 77 + playerbox.Count * 77, 137 );
+                    p3.ImageLocation = card.Image;
+                    p3.SizeMode = PictureBoxSizeMode.AutoSize;
+                    this.Controls.Add(p3);
+
+                    playerbox.Add(p3);
+
+                    playercardList.Add(card);
+                    sumPlayerCards();
+                    if (playercardSum > 21)
                     {
                         resultLabel.Text = String.Format
-                            ("The sum of the cards is: {0}, you win!", cardSum);
+                            ("The sum of your cards is: {0}, you lose!", playercardSum);
+                        MessageBox.Show("You lose!", "", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                        resetGame();
                     }
-                    else
+                    else if (playercardSum == 21)
+                    {
                         resultLabel.Text = String.Format
-                            ("The sum of the cards is: {0}", cardSum);
+                            ("The sum of your cards is: {0}, you win!", playercardSum);
+                        MessageBox.Show("You win!", "", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                        resetGame();
+                    }
                 }
             }
         }
@@ -256,12 +334,26 @@ namespace Blackjack
 
             displayCardBack(pictureBox1);
             displayCardBack(pictureBox2);
-            displayCardBack(pictureBox3);
-            
-            cardSum = 0;
-            cardList.Clear();
+            //displayCardBack(pictureBox3);
+            displayCardBack(pictureBox4);
+            //displayCardBack(pictureBox6);
+            foreach(PictureBox pb in playerbox)
+            {
+                this.Controls.Remove(pb);
+            }
+            playerbox = new List<PictureBox>();
+            foreach (PictureBox pb in bankerbox)
+            {
+                this.Controls.Remove(pb);
+            }
+            bankerbox = new List<PictureBox>();
+
+            playercardSum = 0;
+            bankercardSum = 0;
+            playercardList.Clear();
+            bankercardList.Clear();
             usedCards.Clear();
-            resultLabel.Text = "Click Deal when ready";
+            resultLabel.Text = "Player choice";
         }
 
         private void displayCardBack(PictureBox picturebox)
@@ -269,5 +361,73 @@ namespace Blackjack
             picturebox.ImageLocation = "b1fv.png";
             picturebox.SizeMode = PictureBoxSizeMode.AutoSize;
         }
+
+        // palyer stop move
+        private void PlayerStopButton_Click(object sender, EventArgs e)
+        {
+            if (playercardSum == 0)
+            {
+                resultLabel.Text = "Click the Start button...";
+                return;
+            }
+            sumBankerCards();
+
+            //banker move
+            while (bankercardSum <= 16)
+            {
+                int randomCard = selectRandomCard();
+                Card card = deck[randomCard];
+                usedCards.Add(randomCard);
+
+                if (usedCards.Contains(randomCard)) randomCard = selectRandomCard();
+                else randomCard = 1 * randomCard;
+
+                PictureBox p4 = new PictureBox();
+                p4.Width = 71;
+                p4.Height = 96;
+                p4.Location = new Point(154 + bankerbox.Count * 77, 12);
+                p4.ImageLocation = card.Image;
+                p4.SizeMode = PictureBoxSizeMode.AutoSize;
+                this.Controls.Add(p4);
+
+
+                bankerbox.Add(p4);
+
+                bankercardList.Add(card);
+                sumBankerCards();
+            }
+
+            if (bankercardSum > 21)
+            {
+                resultLabel.Text = String.Format
+                    ("The sum of banker cards is: {0}, you lose!", bankercardSum);
+                MessageBox.Show("You win!", "", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                resetGame();
+            }
+            else if (playercardSum <= bankercardSum)
+            {
+                resultLabel.Text = String.Format
+                    ("The sum of your cards is: {0}, you lose!", playercardSum);
+                MessageBox.Show("You lose!", "", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                resetGame();
+            }
+            else
+            {
+                resultLabel.Text = String.Format
+                    ("The sum of your cards is: {0}, you win!", playercardSum);
+                MessageBox.Show("You win!", "", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                resetGame();
+            }
+
+        }
+
+
+
+
+
+
+
+
+        
     }
 }
